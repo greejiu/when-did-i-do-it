6814fd1d54fc4735aa18439e64d58abcb43b90c1
greejiu <monggreee@naver.com>
2026-08-24 16:25:36 +0900

    refactor: remove obsolete subitems bootstrap

--- js/subitems-bootstrap.js
+++
@@ -1,8 +0,0 @@
-import "./theme-settings.js?v=1";
-import "./theme-refine.js?v=1";
-import "./scroll-preserve.js?v=1";
-import "./edit-view-preserve.js?v=1";
-
-// item cards already receive data-item-id when items.js renders them.
-// Keep this bootstrap focused on loading the subitem module only.
-await import("./subitems.js?v=3");
