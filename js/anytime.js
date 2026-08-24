ef1343a78b2d503e529157318c73637eb0998d5c
greejiu <monggreee@naver.com>
2026-08-24 16:35:13 +0900

    refactor: remove duplicate anytime imports

--- js/anytime.js
+++ js/anytime.js
@@ -1,6 +1,3 @@
-import "./item-taxonomy-quick.js?v=1";
-import "./sorting.js?v=1";
-
 const repeatTypeSelect = document.querySelector("#repeat-type");
 const nextDueInput = document.querySelector("#next-due-override");
 const itemForm = document.querySelector("#item-form");
